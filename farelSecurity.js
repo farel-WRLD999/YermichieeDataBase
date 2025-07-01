fe92b73c2486da7244d3ec2ed84c6812b184a313
farel-WRLD999 <farelwrld@gmail.com>
2025-07-01 14:40:05 +0800

    farelSecurity.js

---
+++ farelSecurity.js
@@ -0,0 +1,71 @@
+{
+  "users": {
+    "6287890234392@s.whatsapp.net": { 
+    "registered": true
+    },
+    "6281376462935@s.whatsapp.net": { 
+    "registered": true
+    },
+    "6285810091758@s.whatsapp.net": { 
+    "registered": true
+    },
+    "6287873540959@s.whatsapp.net": { 
+    "registered": true
+    },
+    "6283122012870@s.whatsapp.net": { 
+    "registered": true
+    },
+    "6287762745232@s.whatsapp.net": { 
+    "registered": true
+    },
+    "6289675799409@s.whatsapp.net": { 
+    "registered": true
+    },
+    "6287873540959@s.whatsapp.net": { 
+    "registered": true
+    },
+    "6285606754374@s.whatsapp.net": { 
+    "registered": true
+    },
+    "6281371531105@s.whatsapp.net": { 
+    "registered": true 
+    }, 
+    "6287762745232@s.whatsapp.net": { 
+    "registered": true 
+    }, 
+    "6289699890494@s.whatsapp.net": {
+    "registered": true
+    },
+    "62895415235000@s.whatsapp.net": { 
+    "registered": true 
+    },
+    "6283801933888@s.whatsapp.net": { 
+    "registered": true 
+    },
+    "6289635801056@s.whatsapp.net": {
+    "registered": true 
+    },
+    "628980639595@s.whatsapp.net": { 
+    "registered": true 
+    },
+    "6282181804515@s.whatsapp.net": { 
+    "registered": true 
+    }, 
+    "6282160476046@s.whatsapp.net": { 
+    "registered": true 
+    },
+    "6281228524770@s.whatsapp.net": { 
+    "registered": true 
+    }, 
+    "6287873540959@s.whatsapp.net": { 
+    "registered": true 
+    },
+    "6283133898447@s.whatsapp.net": { 
+    "registered": true 
+    },
+    "6283898813980@s.whatsapp.net": { 
+    "registered": true 
+    }
+    
+  }
+}
